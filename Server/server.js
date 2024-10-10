e4dcb0bebbfc2476c78c37c97fa24082cd7d0e33
Aldous Roy <aldousroy@Aldouss-MacBook-Air.local>
2024-10-10 12:00:51 +0530

    inital setup

---
+++ Server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app=express();
+const PORT = process.env.PORT || 8000;
+const bodyParser=require('body-parser');
+
+app.get("/",(req,res)=>{
+    res.send("Server is running")
+})
+
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+})
